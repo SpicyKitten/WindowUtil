bf18377556f7e0e70cc871b1b7399d1506f22201
SpicyKitten <rathavilash@gmail.com>
2019-09-28 17:06:34 -0500

    Test dependencies taken off the module path

--- WindowUtil/src/module-info.java
+++ WindowUtil/src/module-info.java
@@ -5,7 +5,6 @@ module avi.utils.window
 	
 	requires java.base;
 	requires avi.utils.throwing;
-	requires org.junit.jupiter.api;
 	requires transitive java.desktop;
 	requires transitive com.sun.jna.platform;
 	requires transitive com.sun.jna;
